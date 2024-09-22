63749f941a6b0bc1fcbff7abb375e628f1d60982
João Pedro Lima <joaopedroca90@gmail.com>
2024-09-22 19:03:34 -0300

    feat: "ADTIntegerDoublyLinkedList implementation with a debugging class"

--- src/entities/ADTDoublyIntegerNode.java
+++ src/entities/ADTDoublyIntegerNode.java
@@ -1,4 +1,32 @@
 package entities;
 
 public class ADTDoublyIntegerNode {
+    private final int ELEMENT;
+    private ADTDoublyIntegerNode next, previous;
+
+    public ADTDoublyIntegerNode(int ELEMENT) {
+        this.ELEMENT = ELEMENT;
+        next = null;
+        previous = null;
+    }
+
+    public int getELEMENT() {
+        return ELEMENT;
+    }
+
+    public ADTDoublyIntegerNode getNext() {
+        return next;
+    }
+
+    public void setNext(ADTDoublyIntegerNode next) {
+        this.next = next;
+    }
+
+    public ADTDoublyIntegerNode getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(ADTDoublyIntegerNode previous) {
+        this.previous = previous;
+    }
 }
--- src/entities/ADTIntegerDoublyLinkedList.java
+++ src/entities/ADTIntegerDoublyLinkedList.java
@@ -1,4 +1,134 @@
 package entities;
 
+import java.util.NoSuchElementException;
+
 public class ADTIntegerDoublyLinkedList {
+    private ADTDoublyIntegerNode start;
+    private int elements;
+
+    public ADTIntegerDoublyLinkedList() {
+        start = null;
+        elements = 0;
+    }
+
+    public boolean isEmpty() {
+        return elements == 0;
+    }
+
+    public void insertAtStart(int element) {
+        ADTDoublyIntegerNode newNode = new ADTDoublyIntegerNode(element);
+
+        if (!isEmpty()) newNode.setNext(start);
+
+        start = newNode;
+        elements++;
+    }
+
+    public void removeAtStart() {
+        if (isEmpty()) throw new NoSuchElementException("List is empty.");
+
+        start = start.getNext();
+        elements--;
+    }
+
+    public void insertAtEnd(int element) {
+        if (isEmpty()) {
+            insertAtStart(element);
+            return;
+        }
+
+        ADTDoublyIntegerNode newNode = new ADTDoublyIntegerNode(element);
+        ADTDoublyIntegerNode currentNode = start;
+
+        while (currentNode.getNext() != null) {
+            currentNode = currentNode.getNext();
+        }
+
+        currentNode.setNext(newNode);
+        newNode.setPrevious(currentNode);
+        elements++;
+    }
+
+    public void removeAtEnd() {
+        if (isEmpty()) throw new NoSuchElementException("List is empty.");
+        if (elements == 1) {
+            start = null;
+            elements--;
+            return;
+        }
+
+        ADTDoublyIntegerNode currentNode = start;
+
+        while (currentNode.getNext() != null) {
+            currentNode = currentNode.getNext();
+        }
+
+        currentNode.getPrevious().setNext(currentNode.getNext());
+        elements--;
+    }
+
+    public void insertAtIndex(int index, int element) {
+        if (index < 0 || index > elements) throw new IndexOutOfBoundsException("Invalid index.");
+        if (index == 0) {
+            insertAtStart(element);
+            return;
+        }
+        if (index == elements) {
+            insertAtEnd(element);
+            return;
+        }
+
+        ADTDoublyIntegerNode newNode = new ADTDoublyIntegerNode(element);
+        ADTDoublyIntegerNode currentNode = start;
+
+        for (int i = 0; i < index - 1; i++) {
+            currentNode = currentNode.getNext();
+        }
+
+        newNode.setNext(currentNode.getNext());
+        newNode.setPrevious(currentNode);
+        currentNode.getNext().setPrevious(newNode);
+        currentNode.setNext(newNode);
+        elements++;
+    }
+
+    public void removeAtIndex(int index) {
+        if (index < 0 || index >= elements) throw new IndexOutOfBoundsException("Invalid index.");
+        if (index == 0) {
+            removeAtStart();
+            return;
+        }
+        if (index == elements - 1) {
+            removeAtEnd();
+            return;
+        }
+
+        ADTDoublyIntegerNode currentNode = start;
+
+        for (int i = 0; i < index - 1; i++) {
+            currentNode = currentNode.getNext();
+        }
+
+        currentNode.setNext(currentNode.getNext().getNext());
+        currentNode.getNext().setPrevious(currentNode);
+        elements--;
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) return "ADTDoublyLinkedList: null <-> null";
+
+        StringBuilder doublyLinkedListString = new StringBuilder("ADTDoublyLinkedList: null <-> ");
+        ADTDoublyIntegerNode currentNode = start;
+
+        while (currentNode.getNext() != null) {
+            doublyLinkedListString.append(currentNode.getELEMENT()).append(" <-> ");
+            currentNode = currentNode.getNext();
+        }
+
+        doublyLinkedListString.append(currentNode.getELEMENT()).append(" <-> null\n");
+        doublyLinkedListString.append("elements: ").append(elements).append("\n");
+
+        return doublyLinkedListString.toString();
+    }
 }
